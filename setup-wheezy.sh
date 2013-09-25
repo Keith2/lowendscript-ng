925e08ed051096ab8d4777ab99cffb3904bab10c
Keith Fernie <keith@fernie.eu>
2013-09-25 15:58:45 +0200

    modified:   setup-wheezy.sh domain option - use return 301 instead of rewrite

--- setup-wheezy.sh
+++ setup-wheezy.sh
@@ -68,17 +68,6 @@ function get_password() {
     echo ${password:0:13}
 }
 
-#function dotdeb {
-#    if [ ! -f /etc/apt/sources.list.d/dotdeb.list ];then
-#         cat > /etc/apt/sources.list.d/dotdeb.list <<END
-#deb http://packages.dotdeb.org stable all
-#deb-src http://packages.dotdeb.org stable all
-#END
-#        wget -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -
-#	print_info "dotdeb repository now being used"
-#    fi
-#}
-
 function install_dash {
     check_install dash "dash"
     rm -f /bin/sh
@@ -580,7 +569,7 @@ END
 	fi
     cat >> "/etc/nginx/sites-available/$2.conf" <<END
 	server_name  www.$2;
-	rewrite ^(.*) http://$2\$1 permanent;
+        return 301 http://$2\$request_uri;
 }
 
 server {
